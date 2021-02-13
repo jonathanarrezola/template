7401e6a3eafad47860304c2e30506921f4d36172
jonathanarrezola <78997143+jonathanarrezola@users.noreply.github.com>
2021-02-12 22:12:38 -0800

    Add files via upload

---
+++ home/home.html.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ace in the hole multisport event template</title>
+    <meta name="description" content="Ace in the hoel multisport events">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/template.css">
+    <link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+    <link href="../styles/reset.css" rel="stylesheet" type="text/css">
+    <link href="../styles/helper.css" rel="stylesheet" type="text/css">
+    <link href="../styles/grid.css" rel="stylesheet" type="text/css">
+    <link href="../styles/styles.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+	<body>
+	<?php include '../includes/header.inc.html.php'; ?>
+	<?php include '../includes/navigation.inc.html.php'; ?>
+		<main>
+			<img src= "../images/images.jpg" alt="Place holder image" class="responsive">
+				<h3>Header 2</h3>
+			<p>Phasellus fermentum porttitor orci, vitae varius lorem varius vel. Etiam sit amet aliquam velit, ut pharetra metus. Nulla at facilisis ligula. Sed iaculis magna sapien, eu pretium libero accumsan at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque mattis lorem sem, at bibendum sem sodales vel. Sed eu sem varius turpis rutrum malesuada.</p>
+			<div id="left">
+				<h4>Header 3</h4>
+			<p>Nunc nibh sapien, eleifend in gravida et, iaculis a risus. Vivamus auctor orci id accumsan dapibus. Nunc ac tortor nibh. Nam odio velit, accumsan eu mollis sit amet, hendrerit at massa. Vivamus congue orci in nulla elementum mattis. Nam rutrum mauris eget nisi elementum, in ultrices justo rhoncus. Proin nec eleifend turpis. Ut velit metus, cursus eu lorem et, pellentesque vehicula tortor. Praesent tincidunt risus ligula, non laoreet justo lacinia ac. Donec ut elit augue. Phasellus et tempor lectus. Mauris congue elementum ligula, a facilisis nibh facilisis vitae. Cras odio felis, fermentum vel purus nec, scelerisque dignissim ligula. Duis at magna et tellus finibus lobortis quis sit amet ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+			<div>
+			<img src="../images/social-feed.jp" alt="Social media feed place holder" class="responsive">
+			<img src="../images/weather.jp" alt="Social media feed place holder" class="responsive">
+		</div>
+				</main>
+		<?php include'../includes/footer.inc.html.php'; ?>
+		<script src="../script/hamburger.js"></script>
+	</body>
+</html>
